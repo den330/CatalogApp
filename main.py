82f11ced5e6273a2b5e39a2e0bd1e640d465b203
vagrant <vagrant@vagrant.vm>
2019-02-09 17:07:20 +0000

    add item to db complete

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, CategoryItem
 # from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///categoryApp.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
 
 
 @app.route("/")
@@ -14,19 +20,37 @@ def catalogList():
 	html += "</body></html>"
 	return html
 
-@app.route("/catalogList/addNewItem")
+@app.route("/catalogList/addNewItem", methods=['GET', 'POST'])
 def addNewItem():
-	html = "<html><body>"
-	html += "<form>"
-	html += "Item Name: <br>"
-	html += "<input type='text', name='Item Name'><br>"
-	html += "Item Category: <br>"
-	html += "<input type='text', name='Item Category'><br>"
-	html += "Item Info: <br>"
-	html += "<input type=text, name='Item Info'><br>"
-	html += "<input type='submit' value='Submit'>"
-	html+= "</body></html>"
-	return html
+	if request.method == 'GET':
+		html = "<html><body>"
+		html += "<form action='/catalogList/addNewItem' method = 'post'>"
+		html += "Item Name: <br>"
+		html += "<input type='text', name='Item Name'><br>"
+		html += "Item Category: <br>"
+		html += "<input type='text', name='Item Category'><br>"
+		html += "Item Info: <br>"
+		html += "<input type=text, name='Item Info'><br>"
+		html += "<input type='submit' value='Submit'>"
+		html += "</form>"
+		html+= "</body></html>"
+		return html
+	else:
+		categoryName = request.form['Item Category']
+		categoryInStoreList = session.query(Category).filter_by(name=categoryName).all()
+		if len(categoryInStoreList) == 0:
+			newCate = Category(name=categoryName)
+			session.add(newCate)
+			session.commit()
+			newItem = CategoryItem(name=request.form['Item Name'], 
+			info=request.form['Item Info'], category_id= newCate.id)
+		else:
+			newItem = CategoryItem(name=request.form['Item Name'], 
+			info=request.form['Item Info'], category_id= categoryInStoreList[0].id)
+		session.add(newItem)
+		session.commit()
+		return redirect(url_for('catalogList'))
+
 	
 if __name__ == '__main__':
     app.debug = True
